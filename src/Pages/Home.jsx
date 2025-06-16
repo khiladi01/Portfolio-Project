b4f0983d7e38eae573074fe5b1360db38cc8e695
raushan778 <allmusiccenter404@gmail.com>
2025-06-17 00:28:43 +0530

    work on 3d model

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
+import "../style/Edit.css"
+import Model from "../components/three";
 
 function Home () {
     return (
         <>
-        <div className="bg-gradient-to-br from-black to-purple-800 min-h-screen flex justify-center item-center">
-        
+        <div className="min-h-screen bg-gradient-to-br from-black to-purple-800 flex justify-center items-center select-none">
+        {/* adding my intro */}
+        <div id="herointro" className="uppercase text-5xl font-bold text-white tracking-normal">
+            <p>raushan</p>
+            <p>raj</p>
+        </div>
+        {/* adding 3d cursor movement robot */}
+        <div className="h-[600px] w-[500px]">
+           <Model />
+        </div>
+        {/* adding my professional */}
+        <div id="herosummary" className="w-[300px] text-white font-normal text-lg">
+            <p>Frontend Dev + 3D Web Enthusiast Building Interfaces with Depth & Detail</p>
+        </div>
         </div>
         </>
     )
